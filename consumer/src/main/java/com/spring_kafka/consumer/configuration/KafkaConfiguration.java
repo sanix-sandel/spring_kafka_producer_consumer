6579f25440d3ca752a94033d860436308fa258a6
sanix <sanicksikani@gmail.com>
2021-11-20 18:40:27 +0300

    ok json

--- consumer/src/main/java/com/spring_kafka/consumer/configuration/KafkaConfiguration.java
+++ consumer/src/main/java/com/spring_kafka/consumer/configuration/KafkaConfiguration.java
@@ -11,6 +11,7 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.converter.StringJsonMessageConverter;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
@@ -42,19 +43,26 @@ public class KafkaConfiguration {
 
     @Bean
     public ConsumerFactory<String, User> userConsumerFactory() {
+
+        JsonDeserializer<User> deserializer = new JsonDeserializer<>(User.class);
+        deserializer.setRemoveTypeHeaders(false);
+        deserializer.addTrustedPackages("*");
+        deserializer.setUseTypeMapperForKey(true);
+
         Map<String, Object> config = new HashMap<>();
 
         config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         config.put(ConsumerConfig.GROUP_ID_CONFIG, "group_json");
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer);
         return new DefaultKafkaConsumerFactory<>(config, new StringDeserializer(),
-                new JsonDeserializer<>(User.class));
+                deserializer);
     }
 
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, User> userKafkaListenerFactory() {
         ConcurrentKafkaListenerContainerFactory<String, User> factory = new ConcurrentKafkaListenerContainerFactory<>();
+       // factory.setMessageConverter(new StringJsonMessageConverter());
         factory.setConsumerFactory(userConsumerFactory());
         return factory;
     }
